f4e2d32ef548465fbea99bb3acc37bd4b49cf385
KartashovIvan <vano.noname69@yandex.ru>
2022-12-01 09:42:40 +0300

    Выполненая экспертная домашка Урок 6

---
+++ src/lesson6/homework/expert/Expert.java
@@ -0,0 +1,8 @@
+package lesson6.homework.expert;
+
+public class Expert {
+    public static void main(String[] args) {
+        Human human = Human.bilder().age(20).name("Ivan").weigh(70).build();
+        human.info();
+    }
+}
