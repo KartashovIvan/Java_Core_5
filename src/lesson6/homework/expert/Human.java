d9ad8a6e44e107e57af465f1408d52954a2aa489
KartashovIvan <vano.noname69@yandex.ru>
2022-12-01 17:36:03 +0300

    Выполненая экспертная домашка Урок 6

--- src/lesson6/homework/expert/Human.java
+++ src/lesson6/homework/expert/Human.java
@@ -5,27 +5,27 @@ public class Human {
     private String name;
     private int weigh;
 
-    public int getAge() {
+    private int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    private void setAge(int age) {
         this.age = age;
     }
 
-    public String getName() {
+    private String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
 
-    public int getWeigh() {
+    private int getWeigh() {
         return weigh;
     }
 
-    public void setWeigh(int weigh) {
+    private void setWeigh(int weigh) {
         this.weigh = weigh;
     }
 
